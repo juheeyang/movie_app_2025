82746daa0508f74dcc8d8cc6e41630c9609de5c0
juheeyang <dmswu11@naver.com>
2025-09-17 17:39:12 +0900

    done

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>I built this pages with nomad react js courses</span>
+      <span>- Juhee Yang</span>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function Detail() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(location);
+  useEffect(() => {
+    if (location.state === null) {
+      navigate("/");
+    }
+  });
+
+  if (location.state) {
+    const { title } = location.state;
+    return <span>{title}</span>;
+  }
+  return null;
+}
+
+export default Detail;
